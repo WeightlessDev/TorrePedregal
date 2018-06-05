47882254bac89715e6fdf824678f1d0816003e07
Denis Garcia <denis.garcia@me.com>
2018-06-05 10:42:14 -0600

    New renders, all rooms

--- data.js
+++ data.js
@@ -1,7 +1,53 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-cocina",
+      "id": "0-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.9768627668451764,
+        "pitch": 0.11760735813545864,
+        "fov": 1.558275891198931
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0902473429707173,
+          "pitch": -0.006155227839803601,
+          "rotation": 0,
+          "target": "1-cocina"
+        },
+        {
+          "yaw": 1.9481316346241249,
+          "pitch": 0.053157732387919765,
+          "rotation": 0,
+          "target": "2-habitacion-master"
+        },
+        {
+          "yaw": 0.17544810443049386,
+          "pitch": 0.09616768492749372,
+          "rotation": 0,
+          "target": "3-habitacion-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cocina",
       "name": "Cocina",
       "levels": [
         {
@@ -20,22 +66,22 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": 2.922693339653529,
-        "pitch": 0.08069743470433721,
+        "yaw": 2.90697763624806,
+        "pitch": 0.08596182694917687,
         "fov": 1.5601080270670864
       },
       "linkHotspots": [
         {
-          "yaw": 0.788069396602399,
-          "pitch": 0.011135935970461475,
+          "yaw": 0.7725559231645391,
+          "pitch": 0.03325005314166063,
           "rotation": 0,
-          "target": "1-habitacion-master"
+          "target": "0-sala"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-habitacion-master",
+      "id": "2-habitacion-master",
       "name": "Habitacion Master",
       "levels": [
         {
@@ -60,20 +106,54 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.8082120152736607,
-          "pitch": 0.04406833754653405,
+          "yaw": -2.7776409447237107,
+          "pitch": 0.08407354662853272,
           "rotation": 0,
-          "target": "0-cocina"
+          "target": "0-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-habitacion-2",
+      "name": "Habitacion 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5488283901652427,
+          "pitch": 0.06399877668946807,
+          "rotation": 0,
+          "target": "0-sala"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Torre Pedregal",
+  "name": "Vistas Del Pedregal",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
